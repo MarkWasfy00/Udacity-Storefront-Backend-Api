92e41e72fa517dec3d3fc47065284c838f69ef25
Mark Wasfy <76422492+MarkWasfy00@users.noreply.github.com>
2022-09-16 06:00:35 +0200

    removed auth from products route

--- src/routes/api/product.ts
+++ src/routes/api/product.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { auth } from "../../middlewares/auth";
 import { idRules, productRules } from "../../middlewares/validation/routesRulesMiddleware";
 import {
   createProduct,
@@ -12,8 +11,8 @@ import { validate } from "../../middlewares/validation/validateMiddleware";
 
 export const productRouter = express.Router();
 
-productRouter.get("/", auth, getProducts);
-productRouter.get("/:id", idRules, validate, auth, showProduct);
+productRouter.get("/", getProducts);
+productRouter.get("/:id", idRules, validate, showProduct);
 
 productRouter.post("/", productRules, validate, createProduct);
 productRouter.put("/:id", idRules, validate, updateProduct);
--- src/tests/productEndpointSpec.ts
+++ src/tests/productEndpointSpec.ts
@@ -2,7 +2,6 @@ import supertest from "supertest";
 import { app } from "../index";
 
 const request = supertest(app);
-let token: string | null = null;
 const product = {
   id: 0,
   name: "mobile",
@@ -10,50 +9,28 @@ const product = {
 };
 
 describe("Testing product routes endpoints", () => {
-  beforeAll(async () => {
-    await request
-      .post("/api/auth/register")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({ email: "testProduct@jasmin.com", firstname: "Jasmin", lastname: "test", password: "jasmine123" });
-    const res = await request
-      .post("/api/auth/login")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({ email: "testProduct@jasmin.com", password: "jasmine123" });
-    token = `${res.body.userToken}`;
-  });
-
-  it("Test create product route  with auth", async () => {
-    const res = await request
-      .post("/api/products")
-      .set("Authorization", `Bearer ${token}`)
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send(product);
+  it("Test create product route", async () => {
+    const res = await request.post("/api/products").send(product);
     expect(res.body.data.name).toEqual(product.name);
     product.id = res.body.data.id;
   });
 
-  it("Test all products route  with auth", async () => {
-    await request.get("/api/products").set("Authorization", `Bearer ${token}`).expect(200);
+  it("Test all products route", async () => {
+    await request.get("/api/products").expect(200);
   });
 
-  it("Test get specific product  with auth", async () => {
-    await request.get(`/api/products/${product.id}`).set("Authorization", `Bearer ${token}`).expect(200);
+  it("Test get specific product", async () => {
+    await request.get(`/api/products/${product.id}`).expect(200);
   });
 
-  it("Test update product route with auth", async () => {
+  it("Test update product route", async () => {
     await request
       .put(`/api/products/${product.id}`)
-      .set("Authorization", `Bearer ${token}`)
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
       .send({ id: product.id, name: "iphone 11", price: 999.99 })
       .expect(200);
   });
 
-  it("Test delete product route with auth", async () => {
-    await request.delete(`/api/products/${product.id}`).set("Authorization", `Bearer ${token}`).expect(200);
+  it("Test delete product route", async () => {
+    await request.delete(`/api/products/${product.id}`).expect(200);
   });
 });
